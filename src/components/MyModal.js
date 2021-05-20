7cd41ef3207e7de95ffee7e5235194dc2d970c09
Razvan <boldearazvan@gmail.com>
2021-05-20 21:20:59 +0300

    frontend: add preventDefault on A event listener

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -7,12 +7,12 @@ export default class MyModal extends Component {
     render() {
         return (
             <div>
-                <Modal show={this.props.show} onHide={() => this.props.onHide()}
+                <Modal show={this.props.show} onHide={() => {this.props.onHide()}}
                 >
                     <Modal.Body className ="modal-body">
                     
                     <div className = "Modal-data-container">
-                        <a href='true' className="Modal-close-button" onClick={() => this.props.onHide()}>
+                        <a href='true' className="Modal-close-button" onClick={(e) => {e.preventDefault(); this.props.onHide(); }}>
                             <span className="closeModalIcon">&times;</span>
                         </a>
                         <div className = "Modal-data">
